bcf094050e87a40e69a0fd895dfbd97fdeeab806
rlatjdus0814 <rlatjdus0814@naver.com>
2020-11-20 12:53:02 +0900

    Updates

---
+++ precache-manifest.53e270d6e20be456085e96184d309a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43b53cec29f222cad6f74e94269db31",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "7cf47e2e5641e516d6df",
+    "url": "/movie_app_2020/static/css/main.b61be6c2.chunk.css"
+  },
+  {
+    "revision": "ca1290ec655463a20b0e",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf47e2e5641e516d6df",
+    "url": "/movie_app_2020/static/js/main.35db9779.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
